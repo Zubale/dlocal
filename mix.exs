1136c19cef6b792c857d62aa9d4006ce5f570748
nmenag <nmena.garzon@gmail.com>
2020-12-15 21:01:42 -0500

    Add ex_doc deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Dlocal.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11-rc",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
       description: description(),
       package: package(),
     ]
@@ -23,8 +23,7 @@ defmodule Dlocal.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.23.0"}
     ]
   end
 
