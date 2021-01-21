77df1600a63a13954282ee3c16bd3b28949ebe22
nmenag <nmena.garzon@gmail.com>
2021-01-21 11:35:19 -0500

    remove dlocal url

--- lib/dlocal/util.ex
+++ lib/dlocal/util.ex
@@ -1,19 +1,20 @@
 defmodule Dlocal.Util do
+  require Logger
   @moduledoc false
 
   @doc false
   @spec get_url() :: String.t()
   def get_url() do
-    if Application.get_env(:dlocal, :sandbox) == "false", do: live_url(), else: sandbox_url()
+    Application.get_env(:dlocal, :url)
   end
 
-  defp live_url do
-    "https://api.dlocal.com/api_curl/cashout_api"
-  end
+  # defp live_url do
+  #   "https://api.dlocal.com/api_curl/cashout_api"
+  # end
 
-  defp sandbox_url do
-    "https://sandbox.dlocal.com/api_curl/cashout_api"
-  end
+  # defp sandbox_url do
+  #   "https://sandbox.dlocal.com/api_curl/cashout_api"
+  # end
 
   def payload_signature(%{} = payload) do
     {_, enc} = Jason.encode(Map.delete(payload, :secret_key))
